9cd8e289ca7e6f91f3cd6d7fe25aa6e1694fc3b4
marchuta <86639141+marchuta@users.noreply.github.com>
2021-11-05 23:30:24 +0900

    tweet保存機能
    new create アクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,27 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @tweet.valid?
+      @tweet.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:image,:sentence).merge(user_id: current_user.id)
+
   end
 
 end
