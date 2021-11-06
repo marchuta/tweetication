6a31885114e2d6aecd56d8cd5542ca7dc770f7d9
marchuta <86639141+marchuta@users.noreply.github.com>
2021-11-06 20:59:15 +0900

    ツイート編集機能　
    追加実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,8 +30,14 @@ class TweetsController < ApplicationController
   end
 
   def update
-    tweet = Tweet.find(params[:id])
-    tweet.update(tweet_params)
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+
   end
 
 
